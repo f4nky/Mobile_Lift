910084699777071d8af2ab0896a8f1d6850c8c43
Christian Meyer <chrigu.meyer@gmail.com>
2014-02-28 10:22:46 +0100

    Fixed problems with a deprecated dependency

--- src/ch/bfh/ti/mobile/fankymeyer/lift/ApplicationManagement.java
+++ src/ch/bfh/ti/mobile/fankymeyer/lift/ApplicationManagement.java
@@ -2,19 +2,21 @@ package ch.bfh.ti.mobile.fankymeyer.lift;
 
 import java.io.IOException;
 
-import ch.quantasy.tinkerforge.tinker.agency.implementation.TinkerforgeStackAgentIdentifier;
+import ch.quantasy.tinkerforge.tinker.agency.implementation.TinkerforgeStackAgency;
+import ch.quantasy.tinkerforge.tinker.agent.implementation.TinkerforgeStackAgentIdentifier;
 import ch.quantasy.tinkerforge.tinker.application.definition.TinkerforgeApplication;
 
 public class ApplicationManagement {
 	public static void main(final String[] args) throws IOException {
 		System.out.println(ApplicationManagement.class.getPackage());
 		final TinkerforgeStackAgentIdentifier identifier = new TinkerforgeStackAgentIdentifier(
-				"MasterBrick3676");
+				"localhost");
 
 		final TinkerforgeApplication application = new MasterBrickApplication();
-		application.addTinkerforgeStackAgent(identifier);
+		TinkerforgeStackAgency.getInstance().getStackAgent(identifier).addApplication(application);
 		System.out.println("Press key to exit");
 		System.in.read();
-		application.removeTinkerforgeStackAgent(identifier);
+		TinkerforgeStackAgency.getInstance().getStackAgent(identifier).removeApplication(application);
+		System.out.println("Finished");
 	}
 }
