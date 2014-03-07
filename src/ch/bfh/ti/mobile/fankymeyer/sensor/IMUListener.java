5d8d44ccfaf1b3905431d76b4f6fa0caaf0bce2c
Christian Meyer <chrigu.meyer@gmail.com>
2014-03-07 12:22:10 +0100

    Improvements: - better gravitational acceleration correction - only z-axis is observed - Removed printlns in order to reduce latency

--- src/ch/bfh/ti/mobile/fankymeyer/sensor/IMUListener.java
+++ src/ch/bfh/ti/mobile/fankymeyer/sensor/IMUListener.java
@@ -29,7 +29,7 @@ public class IMUListener implements AccelerationListener {
 
 		// Remove gravitational acceleration. This is crude, but interestingly
 		// enough the z value seems to have the smallest error.
-		z += 1000;
+		z += 993;
 
 		// Acceleration in m/s^2
 		double aX = x * 9.80665 / 1000;
@@ -38,26 +38,29 @@ public class IMUListener implements AccelerationListener {
 
 		viewer.showAcceleration(aX, aY, aZ);
 
-		System.out.println("x = " + aX);
-		System.out.println("y = " + aY);
-		System.out.println("z = " + aZ);
+		// System.out.println("x = " + aX);
+		// System.out.println("y = " + aY);
+		// System.out.println("z = " + aZ);
 
 		if (lastTime != 0) {
 			double t = (time - lastTime) / 1000.0;
 
 			// Distance traveled in each direction
-			dX += vX * t + aX * t * t / 2;
-			dY += vY * t + aY * t * t / 2;
-			dZ += vZ * t + aZ * t * t / 2;
+			// dX += vX * t + aX * t * t / 2;
+			// dY += vY * t + aY * t * t / 2;
+			dZ += vZ * t + aZ * t * t / 2.0;
 
 			// Velocity in m/s after accelerating
-			vX += aX * t;
-			vY += aY * t;
+			// vX += aX * t;
+			// vY += aY * t;
 			vZ += aZ * t;
 
-			System.out.println("dx = " + dX + "\ndy = " + dY + "\ndz = " + dZ);
+			// System.out.println("dx = " + dX + "\ndy = " + dY + "\ndz = " +
+			// dZ);
+			// System.out.println("t = " + t);
 
-			viewer.showDistance(Math.sqrt(dX * dX + dY * dY + dZ * dZ));
+			// viewer.showDistance(Math.sqrt(dX * dX + dY * dY + dZ * dZ));
+			viewer.showDistance(dZ);
 		}
 		lastTime = time;
 	}
